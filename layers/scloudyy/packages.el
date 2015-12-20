a04074571eda01e0f1a6f153fdc20ca9162981f8
scloudyy <onecloud.shen@gmail.com>
2015-12-20 15:06:09 +0800

    add org insert src

--- layers/scloudyy/packages.el
+++ layers/scloudyy/packages.el
@@ -353,6 +353,33 @@
     (evil-define-key 'normal evil-org-mode-map (kbd "M-j") nil)
     (evil-define-key 'normal evil-org-mode-map (kbd "M-K") 'org-metaup)
     (evil-define-key 'normal evil-org-mode-map (kbd "M-k") nil)
+
+    ;; http://wenshanren.org/?p=327
+    ;; change it to helm
+    (defun zilongshanren/org-insert-src-block (src-code-type)
+      "Insert a `SRC-CODE-TYPE' type source code block in org-mode."
+      (interactive
+       (let ((src-code-types
+              '("emacs-lisp" "python" "C" "sh" "java" "js" "clojure" "C++" "css"
+                "calc" "asymptote" "dot" "gnuplot" "ledger" "lilypond" "mscgen"
+                "octave" "oz" "plantuml" "R" "sass" "screen" "sql" "awk" "ditaa"
+                "haskell" "latex" "lisp" "matlab" "ocaml" "org" "perl" "ruby"
+                "scheme" "sqlite" "yaml")))
+         (list (ido-completing-read "Source code type: " src-code-types))))
+      (progn
+        (newline-and-indent)
+        (insert (format "#+BEGIN_SRC %s\n" src-code-type))
+        (newline-and-indent)
+        (insert "#+END_SRC\n")
+        (previous-line 2)
+        (org-edit-src-code)))
+
+    (add-hook 'org-mode-hook '(lambda ()
+                                ;; keybinding for editing source code blocks
+                                ;; keybinding for inserting code blocks
+                                (local-set-key (kbd "C-c i s")
+                                               'zilongshanren/org-insert-src-block)
+                                ))
     )
   )
 
